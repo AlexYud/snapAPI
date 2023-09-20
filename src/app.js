100877a3fbdc071c5685db3b56861dc0919d05e9
Alexandre Yudi <alexyud@hotmail.com>
2023-09-20 16:28:51 -0300

    Adding more tests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 let mongoose = require('mongoose');
+let user = require('./models/user');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -11,12 +12,32 @@ mongoose.connect("mongodb://0.0.0.0:27017/snapAPI", { useNewUrlParser: true, use
   console.log(err);
 });
 
+let User = mongoose.model("User", user);
+
 app.get('/', (req, res) => {
   res.json({});
 });
 
-app.post("/user", (req, res) => {
-  res.json({});
+app.post("/user", async (req, res) => {
+  let { name, email, password } = req.body;
+
+  if (name === "" || email == "" || password == "") return res.sendStatus(400);
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      res.statusCode = 400;
+      res.json({ error: "Email já cadastrado" });
+      return
+    }
+
+    let newUser = new User({ name, email, password });
+    await newUser.save();
+    res.json({ email });
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 module.exports = app;
